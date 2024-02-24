06baf040e531515ec0b9434bafeee00ee48165dc
Peihong Yu <lemon87123@gmail.com>
2024-02-24 16:09:22 -0500

    update batch train scripts

--- src/run_batch_train.py
+++ src/run_batch_train.py
@@ -6,12 +6,12 @@ from types import SimpleNamespace as SN
 
 # Define the maximum number of jobs for each account
 # max_job_nums = [4, 2, 2, 8]
-max_job_nums = [8]
+max_job_nums = [2, 8]
 account_combinations = [
     # ["nexus", "tron", "medium"],      # 2 days, for qmix, 4 jobs max
     ["cml-tokekar", "cml-dpart", "cml-medium"],     # 3 days, for ippo/dm2, 2 jobs max
     # ["cml-tokekar", "cml-dpart", "cml-high"],     # 1.5 days, for qmix, 2 jobs max
-    # ["cml-tokekar", "cml-dpart", "cml-high_long"],    # 14 days, for ippo/dm2, 8 jobs max, but actually 2 jobs max due to resources
+    ["cml-tokekar", "cml-dpart", "cml-high_long"],    # 14 days, for ippo/dm2, 8 jobs max, but actually 2 jobs max due to resources
 ]
 
 # Define the parameters you want to iterate over
@@ -23,6 +23,7 @@ account_combinations = [
 # }
 parameters = {
     "seed": [112358, 1285842, 78590, 119527, 122529],
+    # "seed": [78590, 119527, 122529],
 }
 
 # root_dir = "/fs/nexus-scratch/peihong/dm2_results"
@@ -37,11 +38,23 @@ param_names = list(parameters.keys())
 param_values = [v for v in parameters.values()]
 combinations = list(product(*param_values))
 
+# algo_name = "qmix"
+# python_command = "python src/main.py --env-config=sc2 --config=default --alg-config=qmix with env_args.map_name=5m_vs_6m t_max=10050000 name='qmix'"
+
+# algo_name = "ippo"
+# python_command = "python src/main.py --env-config=sc2 --config=default_ippo_5v6 --alg-config=ippo with env_args.map_name=5m_vs_6m rew_type='env' update_gail=False t_max=10050000 name='ippo'"
+
+# algo_name = "dm2"
+# python_command = "python src/main.py --env-config=sc2 --config=default_ippo_5v6 --alg-config=ippo with env_args.map_name=5m_vs_6m rew_type='mixed' update_gail=True t_max=10050000 name='dm2'"
+
+algo_name = "dm2_sa"
+python_command = "python src/main.py --env-config=sc2 --config=default_ippo_5v6 --alg-config=ippo with env_args.map_name=5m_vs_6m rew_type='mixed' update_gail=True t_max=10050000 gail_state_discrim=False name='dm2_sa'"
+
 # Iterate over parameter combinations
 jobs_num = 0
 for combo in combinations:
     job_name = "__".join([f"{name}_{value}" for name, value in zip(param_names, combo)])
-    job_name = "dm2__" + job_name
+    job_name = f"{algo_name}__{job_name}"
     param_dict = {key: value for key, value in zip(param_names, combo)}
     param = SN(**param_dict)
 
@@ -59,7 +72,7 @@ for combo in combinations:
     job_script_content = f'''#!/bin/bash
 #SBATCH --job-name={job_name}
 #SBATCH --output={root_dir}/slurm_logs/%x.%j.out
-#SBATCH --time=38:00:00
+#SBATCH --time=40:00:00
 #SBATCH --account={account}
 #SBATCH --partition={partition}
 #SBATCH --qos={qos}
@@ -77,7 +90,7 @@ conda activate dm2
 export SC2PATH={smac_dir}
 
 # Your Python script with parameters
-srun bash -c "python src/main.py --env-config=sc2 --config=default_ippo_5v6 --alg-config=ippo with env_args.map_name=5m_vs_6m rew_type='env' update_gail=False t_max=10050000 name='ippo' --seed={param.seed}"
+srun bash -c "{python_command} --seed={param.seed}"
 ''' 
 
 # python src/main.py --env-config=sc2 --config=default --alg-config=qmix with env_args.map_name=5m_vs_6m t_max=10050000 name='qmix' --seed=112358
